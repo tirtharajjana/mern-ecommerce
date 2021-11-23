89fc2139d3d7b40aa1c542335fecfffe0a9e2f10
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-11-23 22:09:01 +0530

    Fix JWT Expire login issue

--- ecommerce-back-end/src/controllers/admin/auth.js
+++ ecommerce-back-end/src/controllers/admin/auth.js
@@ -33,8 +33,8 @@ exports.signin = (req, res) => {
                 return res.status(400).json({ error })
             if (user) {
                 if (user.authenticate(req.body.password) && user.role === "admin") {
-                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRTE, { expiresIn: '10d' });
-                    res.cookie('token', token, { expiresIn: '10d' })
+                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRTE, { expiresIn: '1h' });
+                    res.cookie('token', token, { expiresIn: '1h' })
                     const { _id, firstName, lastName, email, role, fullName } = user;
                     res.status(200).json({ token, user: { _id, firstName, lastName, email, role, fullName } })
                 } else {
