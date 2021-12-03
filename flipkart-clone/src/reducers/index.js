f4ff53cc63c30607fe7982081307b63fd6d643c4
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-12-03 11:53:52 +0530

    Flipkart Header Clone - 30

--- flipkart-clone/src/reducers/index.js
+++ flipkart-clone/src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 import categoryReducre from './category.reducer';
 import productReducre from './product.reducer';
-
+import authReducer from "./auth.reducer";
 const rootReducre = combineReducers({
 
   category: categoryReducre,
-  product: productReducre
+  product: productReducre,
+  auth: authReducer
 
 })
 
