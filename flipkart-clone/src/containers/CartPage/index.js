1a3621cc4d9cdc7b5caffdae5713db4296089daf
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-12-06 22:24:55 +0530

    Increase Cart Quantity

--- flipkart-clone/src/containers/CartPage/index.js
+++ flipkart-clone/src/containers/CartPage/index.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import Layout from '../../components/Layout'
 import Card from '../../components/UI/Card';
+import { generatePublicUrl } from '../../urlConfig';
+import CartItem from './CartItem';
+import { addToCart } from '../../actions';
 
 import './style.css';
 
@@ -10,54 +13,57 @@ import './style.css';
 * @function CartPage
 **/
 
-/*
-Before Login
-Add product to cart
-save in localStorage
-when try to checkout ask for credentials and 
-if logged in then add products to users cart database from localStorage
-*/
 
-const CartPage = (props) => {
+
+const CartPage = () => {
 
     const cart = useSelector(state => state.cart);
-    const cartItems = cart.cartItems;
+    // const cartItems = cart.cartItems;
+    const [cartItems, setCartItems] = useState(cart.cartItems);
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        setCartItems(cart.cartItems);
+    }, [cart.cartItems]);
 
+    const onQuantityIncrement = (_id, qty) => {
+        //console.log({_id, qty});
+        const { name, price, img } = cartItems[_id];
+        dispatch(addToCart({ _id, name, price, img }, 1));
+    }
+
+    const onQuantityDecrement = (_id, qty) => {
+        const { name, price, img } = cartItems[_id];
+        dispatch(addToCart({ _id, name, price, img }, -1));
+    }
     return (
         <Layout>
-            <div className="cartContainer">
+            <div className="cartContainer" style={{ alignItems: 'flex-start' }}>
                 <Card
                     headerLeft={`My Cart`}
                     headerRight={<div>Deliver to</div>}
                 >
                     {
                         Object.keys(cartItems).map((key, index) =>
-                            <div key={index} className="flexRow">
-
-                                <div className="cartProductContainer">
-                                    <img src="" alt='' />
-                                </div>
-                                <div className="cartItemDetails">
-                                    <div>
-                                        {cartItems[key].name} - qty - {cartItems[key].qty}
-                                    </div>
-                                    <div>Delivery in 3 - 5 days</div>
-                                </div>
-
-                            </div>
-
+                            <CartItem
+                                key={index}
+                                cartItem={cartItems[key]}
+                                onQuantityInc={onQuantityIncrement}
+                                onQuantityDec={onQuantityDecrement}
+                            />
                         )
                     }
+                </Card>
+                <Card
+                    headerLeft='Price'
+                    style={{
+                        width: '500px'
+                    }}>
 
                 </Card>
-                <Card style={{
-                    width: '500px'
-                }}>Price</Card>
             </div>
         </Layout>
     )
-
 }
 
 export default CartPage
