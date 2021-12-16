cfea0b65a7d4a14aaed134dd51a97f68cece9072
Tirtharaj Jana <55793707+tirtharajjana@users.noreply.github.com>
2021-12-16 10:16:08 +0530

    Admin Orders handling - 44

--- flipkart-clone/src/helpers/axios.js
+++ flipkart-clone/src/helpers/axios.js
@@ -1,14 +1,16 @@
-import axios from 'axios';
-import { api } from '../urlConfig';
-import store from '../store';
 
-const token = window.localStorage.getItem('token');
+import axios from "axios";
+import { api } from "../urlConfig";
+import store from "../store";
+import { authConstants } from "../actions/constants";
+
+const token = window.localStorage.getItem("token");
 
 const axiosIntance = axios.create({
     baseURL: api,
     headers: {
-        'Authorization': token ? `Bearer ${token}` : ''
-    }
+        Authorization: token ? `Bearer ${token}` : "",
+    },
 });
 
 axiosIntance.interceptors.request.use((req) => {
@@ -17,6 +19,21 @@ axiosIntance.interceptors.request.use((req) => {
         req.headers.Authorization = `Bearer ${auth.token}`;
     }
     return req;
-})
+});
+
+axiosIntance.interceptors.response.use(
+    (res) => {
+        return res;
+    },
+    (error) => {
+        console.log(error.response);
+        const status = error.response ? error.response.status : 500;
+        if (status && status === 500) {
+            localStorage.clear();
+            store.dispatch({ type: authConstants.LOGOUT_SUCCESS });
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default axiosIntance;
